4865acb407ba06d7238eec7050eaa376480b0547
kye8524 <kye8314@gmail.com>
2020-12-02 17:51:09 +0900

    공지사항 하는중

--- GOWEB_Back/app.js
+++ GOWEB_Back/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var mysqlRouter = require('./routes/mysql');
+var noticeRouter = require('./routes/notice');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/mysql', mysqlRouter);
+app.use('/notice', noticeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ GOWEB_Back/routes/notice.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var mysql_odbc = require('../database/db_conn')();
+var conn = mysql_odbc.init();
+
+router.get('/list', function(req, res, next) {
+    res.redirect('/notice/Notice_list/1')
+});
+
+router.get('/Notice_list/:page', function(req, res, next) {                                                //uri를 'list/:page'형태로 받음.board/list/(페이지숫자)형식으로 게시판리스트 노출
+    var page = req.params.page;                                                                     //uri변수 'page'로 맴핑된 page값을 req객체로 가져옴 > 페이징개발을 위해 미리 선언함
+    var sql = "select idx, title," +     //sql문 수행
+        "date_format(regdate,'%Y-%m-%d %H:%i:%s') regdate from board";
+    conn.query(sql, function (err, rows) {                                                   //select된 행을 가져와서 rows 변수에 담는다.오류가 있다면 err에 담는다.
+        if (err) console.error("err : " + err);
+        res.render('Notice_list', {rows: rows});                               //수행된 sql에 데이터를 list뷰로 랜더링함.
+    });
+});
+
+module.exports = router;
