49c2f17251848aecf425c515300ae61f7a21b3d6
yossogood8611 <rooproop1111@naver.com>
2020-12-07 17:01:17 +0900

    jquery 적용 오류 menu 수정 banner list 수정

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -4,3 +4,43 @@ function show() {
 function hide() {
     document.getElementById("menu").style.visibility = "hidden";
 }
+
+(function(){
+    var current = 0;
+    var max = 0;
+    var container;
+    var interval;
+
+    function init (){
+        container = jQuery(".Banner ul");
+        max = container.children().length;
+        events();
+
+        console.log('init')
+
+        interval = setInterval(next, 3000);
+    }
+    function events(){
+        jQuery("button.left_btn").on("click", prev);
+        jQuery("button.right_btn").on("click", next);
+    }
+
+    function prev ( e ){
+        current--;
+        if( current<0 ) current = max-1;
+        animate();
+    }
+    function next( e ){
+        current++;
+        if(current>max-1) current = 0;
+        animate();
+    }
+    function animate(){
+        const moveX = current * 1200;
+        TweenMax.to( container, 0.8, { marginLeft: -moveX, ease: Expo.easeOut });
+
+        clearInterval(interval);
+        interval = setInterval(next, 3000);
+    }
+    $(window).load(init);
+})();
