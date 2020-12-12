5e80ab073217af76f08b2bd61c2a00a930f41555
yossogood8611 <rooproop1111@naver.com>
2020-12-12 16:25:47 +0900

    Sign_up javascript, UserType 보내기 완성 - 확인 필요 Charge javascript 완성

--- public/javascripts/Sign_up.js
+++ public/javascripts/Sign_up.js
@@ -1,11 +1,15 @@
+var pur;
+
 function check_donor() {
     document.getElementById("donor").style.border="4px solid #30558A";
     document.getElementById("donor").style.boxShadow="3px 4px 6px gray;";
+    pur = {UserType: "donor"};
 }
 
 function check_charity() {
     document.getElementById("charity").style.border="4px solid #30558A";
     document.getElementById("charity").style.boxShadow="3px 4px 6px gray;";
+    pur = {UserType:"charity"};
 }
 
 function check_out_donor() {
@@ -26,5 +30,44 @@ function check_pwd() {
         return false;
     }else{
         return true;
+    }
+}
+
+function UserType() {
+    if(pur===0){
+        document.UserType.focus();
+        return false;
+    }else{
+        return true;
+    }
+}
+
+function doAction() {
+    var req = createRequest();
+    req.open("post", "Sign_up.jsp?param="+encodeURI(pur));
+    req.setRequestHeader("UserType", "XMLHttpRequest");
+    req.send(pur);
+}
+
+function createRequest() {
+    var httplist = [
+        function () {
+            return new XMLHttpRequest();
+        },
+        function () {
+            return new ActiveXObject("Msxm12.XMLHTTP");
+        },
+        function () {
+            return new ActiveXObject("Microsoft.XMLHTTP");
+        }
+    ];
+    for (var i = 0; i < httplist.length; i++) {
+        try {
+            var http = httplist[i]();
+            if (http != null) return http;
+        } catch (e) {
+            continue;
+        }
+        return null;
     }
 }
---
+++ public/javascripts/charge.js
@@ -0,0 +1,6 @@
+function change_krw() {
+    var coin = document.getElementById("coin_charge").value;
+    var kr = 100 * coin;
+    document.getElementById("krw_").innerHTML = kr;
+}
+
