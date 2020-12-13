0103ae85bcfa38270a4be32eef9348f737fe9d88
Jwoong <dbswodnd5428@gmail.com>
2020-12-14 00:40:21 +0900

    charge 만듬

--- routes/charge.js
+++ routes/charge.js
@@ -9,7 +9,30 @@ const crypto = require('crypto');
 const cUtil = require('../customUtil');
 
 router.get('/', function(req, res, next) {
-    res.sendFile(path.join(__dirname+'/../html/charge.html'));
+    var userSeq=req.params.userSeq;
+    var sql="select totalCoin from userInfo where userSeq=?;";
+    conn.query(sql,userSeq,function (err,results){
+        if(err)console.error('err : ' + err);
+        res.render('/',{result:results});
+        res.sendFile(path.join(__dirname+'/../html/charge.html'));
+    })
 });
 
+router.post('/',function (req,res,next){
+    var userSeq=req.params.userSeq;
+    var charge = req.body.coin_charge;
+    const sql1 = "SELECT userSeq FROM UserInfo WHERE userSeq=?;";
+    conn.query(sql1,userSeq,function (err,rows,field){
+        if(err){
+            console.log(err);
+        }
+        else{
+            const sql2="UPDATE userInfo set totalCoin=totalCoin+charge where userSeq=?;";
+            conn.query(sql2,userSeq,function (err){
+                if(err)console.log(err);
+                else res.redirect('/');
+            })
+        }
+    })
+})
 module.exports = router;
