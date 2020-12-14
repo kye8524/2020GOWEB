83a803b36aa8bbda35957e6dd68bd2bfc8b3bb09
kye8524 <kye8314@gmail.com>
2020-12-14 15:54:34 +0900

    로그인 구현

--- routes/auth.js
+++ routes/auth.js
@@ -19,55 +19,26 @@ router.post('/login', obtainToken);
 
 function obtainToken(req, res) {
     const email = req.body.email;
-    const reqPw = req.body.passwd;
+    const passwd = req.body.passwd;
 
-    const sql = "SELECT * FROM UserInfo WHERE email = ?";
-    const sql4 = "update UserInfo set accessToken = ? where email = ?";
-    const sql1 = mysql.format(sql, email);
-    conn.query(sql1, function (err, rows) {
-        if (err) {
-            console.log("SELECT ERROR : " + err);
-            res.status(500).send('500 SERVER ERROR');
-        } else if (rows.length === 0) {
-            console.log("NO ACCOUNT");
-            res.status(204).send('NO ACCOUNT');
-        } else {
-            const dbPw = rows[0].passwd;
-            const salt = rows[0].salt;
-            pwBySalt(reqPw, salt).then(function (resolve) {
-                const crypReqPw = resolve;
-                if (dbPw !== crypReqPw) {
-                    console.log("uncorrect pw : " + resolve);
-                    res.status(204).send('UNCORRECT PW');
-                } else {
-                    let timestamp = new Date().getTime();
-                    console.log("timeStamp : " + timestamp);
-                    let token = jwt.sign(email, timestamp.toString(16), {
-                        algorithm: 'HS256'
-                    });
-                    let accessTokenSubStr = token.substr(0, 64);
-                    conn.query(sql4, [accessTokenSubStr, email], function (error, results, nexts) {
-                        if (error) {
-                            console.log(error);
-                            res.status(400).send("insert token error");
-                        } else {
-                            console.log("insert token");
-                            conn.query(sql, email, function (errors, result, field) {
-                                if (errors) {
-                                    console.log("select error");
-                                    res.status(400).send("token error");
-                                } else {
-                                    console.log("login success");
-                                    res.status(200).send(result[0]);
-                                }
-                            })
-                        }
-                    })
-                }
-            })
-        }
-    })
+    const sql = "SELECT * FROM UserInfo WHERE email = ? AND passwd=?";
+    if (email && passwd) {
+        conn.query(sql, [email,passwd], function(error, results, fields) {
+            if (error) throw error;
+            if (results.length !== 0) {
+                res.redirect('/index');
+                res.end();
+            } else {
+                console.log('Incorrect');
+                res.send("Incorrect Username and/or Password!");
+            }
+        });
+    } else {
+        res.send('Please enter Username and Password!');
+        res.end();
+    }
 }
+
 router.post('/register',function (req,res,next){
     var email = req.body.email;
     var passwd = req.body.passwd;
