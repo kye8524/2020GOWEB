8ddb6ca8a8cfad8967644f49c69953f632ce227d
kye8524 <kye8314@gmail.com>
2020-12-15 22:57:50 +0900

    중복 오류수정

--- public/javascripts/Project_name.js
+++
@@ -1,8 +0,0 @@
-var name;
-
-function saveName() {
-    name = document.getElementById("project_name").value;
-    var name_project = document.getElementById("name_project");
-    name_project.innerText = name;
-}
-
