a485d6327a7c98633c41249dbe6a0f757ea5d5c6
kye8524 <kye8314@gmail.com>
2020-12-18 01:21:02 +0900

    donation coin localStorage 구현

---
+++ public/javascripts/donate_coin_get.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+    document.getElementById("donateCoin_").innerText = localStorage.getItem('name2') ? localStorage.getItem('name2') : "default";
+});
---
+++ public/javascripts/donate_coin_set.js
@@ -0,0 +1,5 @@
+function setItem1() {
+    var donateCoin = document.getElementById("donateCoin").value;
+    localStorage.setItem('name2', donateCoin);
+    fetch('/')
+}
