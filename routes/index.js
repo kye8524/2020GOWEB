16c79d6a24a6a2be0bfb7a747978988764e07f64
Jwoong <dbswodnd5428@gmail.com>
2020-12-17 21:55:34 +0900

    index 공지사항

--- routes/index.js
+++ routes/index.js
@@ -7,18 +7,21 @@ var session = require('express-session');
 
 
 router.get('/', function(req, res, next) {
-    if(req.cookies.accessToken){
-        var userinfo = req.userInfo;
-        if(userinfo){
-            let userType=userinfo.userType;
-            userType='/mypage/'+userType;
-            console.log(userType);
-            res.render('index',{link:userType,val1:'마이메이지',val2:'로그아웃'});
+    var sql="select * from notice";
+    conn.query(sql,function (err,rows){
+        if(err) console.log(err);
+        if(req.cookies.accessToken){
+            var userinfo = req.userInfo;
+            if(userinfo){
+                let userType=userinfo.userType;
+                userType='/mypage/'+userType;
+                res.render('index',{rows:rows,link:userType,val1:'마이메이지',val2:'로그아웃'});
+            }
+        }else {
+            console.log('cookie none');
+            res.render('index',{rows:rows,link:'/auth/register',val1:'회원가입',val2:'로그인'});
         }
-    }else {
-        console.log('cookie none');
-        res.render('index',{link:'/auth/register',val1:'회원가입',val2:'로그인'});
-    }
+    })
 });
 router.get('/introduce',function (req,res,next){
     if(req.cookies.accessToken){
